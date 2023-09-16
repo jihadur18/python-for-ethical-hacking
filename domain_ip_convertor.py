5e2416801140837d1a471df22c7c73cabb40a86c
Md. Jihadur Rahman <98876851+jihadur18@users.noreply.github.com>
2023-09-16 11:44:13 +0600

    Add files via upload

---
+++ domain_ip_convertor.py
@@ -0,0 +1,17 @@
+
+from termcolor import colored
+
+print (colored("********************Domain To IP Convertor****************",'green'))
+print(colored("*********************Create By 0xanon_hash***************",'red'))
+
+import socket
+
+import pyfiglet
+
+banner = colored(pyfiglet.figlet_format("Domain To IP"),'green')
+print(banner)
+domain_name = input("Enter yur target domain:")
+ip = socket.gethostbyname(domain_name)
+print("IP for {} : {}".format(domain_name,ip))
+
+
